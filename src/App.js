8ef53554dbb635f5086efb016527c151d58003ba
Debabrata23 <debabratash0@gmail.com>
2022-10-01 00:56:06 +0530

    ch 3

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ import TopNavigation from './components/TopNavigation';
 import Home from './components/Home/Home';
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {HashRouter} from 'react-router-dom';
 import Certifications from './components/Certifications/Certifications';
 
 import WorkExperience from './components/WorkExperience/WorkExperience';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
     <Routes>
     
         <Route  path="/" element={<Home />} />
@@ -20,7 +21,7 @@ function App() {
       
      
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
     // <React.Fragment>
     // <TopNavigation></TopNavigation>
     
